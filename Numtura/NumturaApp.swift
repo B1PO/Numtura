1c534c6595b86d108ce13d93512b712afd80dbac
Pedro Prado <pedroprado1418@gmail.com>
2023-09-13 18:51:32 -0600

    Initial Commit

---
+++ Numtura/NumturaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumturaApp.swift
+//  Numtura
+//
+//  Created by Pedro Prado on 13/09/23.
+//
+
+import SwiftUI
+
+@main
+struct NumturaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
