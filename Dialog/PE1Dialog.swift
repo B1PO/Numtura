256b7d23613fafa999374a400db59ea8991673ca
Pedro Prado <pedroprado1418@gmail.com>
2023-11-07 10:26:52 -0600

    ExGeo

---
+++ Dialog/PE1Dialog.swift
@@ -0,0 +1,18 @@
+//
+//  PE1Dialog.swift
+//  Numtura
+//
+//  Created by Pedro Prado on 25/10/23.
+//
+
+import SwiftUI
+
+struct PE1Dialog: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PE1Dialog()
+}
---
+++ Exercises/PhysicsEx2View.swift
@@ -0,0 +1,18 @@
+//
+//  PhysicsEx2View.swift
+//  Numtura
+//
+//  Created by Pedro Prado on 25/10/23.
+//
+
+import SwiftUI
+
+struct PhysicsEx2View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PhysicsEx2View()
+}
