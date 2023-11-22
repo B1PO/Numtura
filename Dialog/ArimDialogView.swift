cc869185b2569646dee2b2aec46aa8a089a414ac
“Daniel <danieleruisl225@gmail.com>
2023-11-21 19:12:33 -0600

    Menu arim

---
+++ Dialog/ArimDialogView.swift
@@ -0,0 +1,18 @@
+//
+//  ArimDialogView.swift
+//  Numtura
+//
+//  Created by Daniel Ruis on 19/11/23.
+//
+
+import SwiftUI
+
+struct ArimDialogView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ArimDialogView()
+}
---
+++ Numtura/menuArim.swift
@@ -0,0 +1,8 @@
+//
+//  menuArim.swift
+//  Numtura
+//
+//  Created by Daniel Ruis on 19/11/23.
+//
+
+import Foundation
