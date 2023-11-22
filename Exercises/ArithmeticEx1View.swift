a5e2f11130e8e68c49004b74b54020194ce3920b
Pedro Prado <pedroprado1418@gmail.com>
2023-11-21 22:14:50 -0600

    Agregados ejercicios Arim parcialmente - Falta revisar los assets

--- Exercises/ArithmeticEx1View.swift
+++ Exercises/ArithmeticEx1View.swift
@@ -1,8 +1,173 @@
-//
-//  ArithmeticEx1View.swift
-//  Numtura
-//
-//  Created by Pedro Prado on 21/11/23.
-//
+import SwiftUI
 
-import Foundation
+struct ArithmeticEx1View: View {
+    @State private var score = 0
+    @State private var timeRemaining = 30
+    @State private var isGameActive = false
+    @State private var stars: [Starrr] = []
+
+    let primeNumbers = [
+        2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
+        31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
+        73, 79, 83, 89, 97, 101, 103, 107, 109, 113,
+        127, 131, 137, 139, 149, 151, 157, 163, 167, 173,
+        179, 181, 191, 193, 197, 199, 211, 223, 227, 229,
+        233, 239, 241
+    ]
+
+    @State private var isShowingMenu = false // Variable para controlar la visualización del menú
+
+    var body: some View {
+        ZStack {
+            BackgroundView()
+                .zIndex(0)
+
+            VStack {
+                Text("Puntuación: \(score)")
+                    .font(.system(size: 40))
+                    .foregroundColor(.white)
+
+                if isGameActive {
+                    Text("Tiempo Restante: \(timeRemaining)")
+                        .font(.title)
+                        .foregroundColor(.white)
+                        .onReceive(Timer.publish(every: 1, on: .main, in: .common).autoconnect()) { _ in
+                            if timeRemaining > 0 {
+                                timeRemaining -= 1
+                                if stars.filter({ $0.isPrime && !$0.isFadingOut }).isEmpty {
+                                    endGame()
+                                }
+                            } else {
+                                endGame()
+                            }
+                        }
+
+                    LazyVGrid(columns: Array(repeating: GridItem(), count: 5),spacing: 90) {
+                        ForEach(stars.indices, id: \.self) { index in
+                            Button(action: {
+                                if stars[index].isPrime {
+                                    if !stars[index].isFadingOut {
+                                        score += 1
+                                        withAnimation {
+                                            stars[index].isFadingOut = true
+                                            stars[index].isGrayedOut = true
+                                        }
+                                        if stars.filter({ $0.isPrime && !$0.isFadingOut }).isEmpty {
+                                            endGame()
+                                        }
+                                    }
+                                } else {
+                                   
+                                }
+                            }) {
+                                ZStack {
+                                    Image(systemName: "star.fill")
+                                        .font(.system(size: 97.75))
+                                        .foregroundColor(stars[index].isGrayedOut ? .gray : .yellow)
+                                    Text("\(stars[index].primeNumber)")
+                                        .font(.title)
+                                        .foregroundColor(.primary) // Cambia el color del texto a negro
+                                }
+                                .scaleEffect(stars[index].isFadingOut ? 0.5 : 1.0)
+                                .opacity(stars[index].isFadingOut ? 0.5 : 1.0)
+                                .animation(.easeInOut(duration: 0.5))
+                            }
+                            .frame(width: 75, height: 75) // Ajusta el tamaño de las estrellas
+                        }
+                    }
+                    .frame(width: 800, height: 1000)
+                    .drawingGroup()
+
+                    // Botón para volver al menú
+    
+                    
+                } else {
+                    
+                    Text("").padding().padding()
+                    Button("Comenzar Juego") {
+                        score = 0
+                        timeRemaining = 30
+                        isGameActive = true
+                        generateStars()
+                    }
+                    .font(.system(size: 40))
+                        .padding()
+                        .foregroundColor(Color.white)
+                        .background(Color.secondary)
+                    
+                    Text("").padding().padding()
+                    
+                    Button("Volver al Menú") {
+                                       isShowingMenu.toggle()
+                                   }
+                    .font(.system(size: 40))
+                                   .padding()
+                                   .foregroundColor(Color.white)
+                                   .background(Color.secondary)
+                                   .fullScreenCover(isPresented: $isShowingMenu, content: {
+                                       StarEx1View()
+                                   })
+                }
+            }
+        }
+    }
+
+    func generateStars() {
+        stars.removeAll()
+        var allStars = [Starrr]()
+        var primeIndex = 0
+        var isNextPrime = true
+
+        while allStars.count < 25 {
+            var primeNumber: Int
+
+            if isNextPrime {
+                // Si todavía hay números primos disponibles en la lista, selecciona uno.
+                if primeIndex < primeNumbers.count {
+                    primeNumber = primeNumbers[primeIndex]
+                    primeIndex += 1
+                } else {
+                    // Si no quedan números primos disponibles, cambia a números pares aleatorios (excluyendo el 2).
+                    repeat {
+                        primeNumber = Int.random(in: 4...100)
+                    } while primeNumber % 2 != 0
+                }
+            } else {
+                // Siempre selecciona números pares (excluyendo el 2) cuando no sea un número primo.
+                repeat {
+                    primeNumber = Int.random(in: 4...100)
+                } while primeNumber % 2 != 0
+            }
+
+            let x = CGFloat.random(in: 0..<5) * 75
+            let y = CGFloat.random(in: 0..<5) * 75
+
+            let newStar = Starrr(isPrime: isNextPrime, primeNumber: primeNumber, x: x, y: y)
+            allStars.append(newStar)
+            isNextPrime.toggle()
+        }
+
+        stars = allStars.shuffled()
+    }
+
+    func endGame() {
+        isGameActive = false
+        
+    }
+}
+
+struct ArithmeticEx1View_Previews: PreviewProvider {
+    static var previews: some View {
+        ArithmeticEx1View()
+    }
+}
+
+struct Starrr: Identifiable {
+    var id = UUID()
+    var isPrime: Bool
+    var primeNumber: Int
+    var x: CGFloat
+    var y: CGFloat
+    var isFadingOut: Bool = false
+    var isGrayedOut: Bool = false
+}
--- Exercises/StarEx1View.swift
+++ Exercises/StarEx1View.swift
@@ -1,1 +1,177 @@
+import SwiftUI
 
+struct StarEx1View: View {
+    @State private var dialogIndex = 0
+
+    var dialogTexts = [
+        "Hola compañero",
+        "Como buen astronauta",
+        "Haz de conocer los números primos",
+        "Vamos a ver qué tan bueno eres identificándolos",
+        "Hay algunos perdidos en las estrellas",
+        "Recuerda:",
+        "Los números primos son números naturales",
+        "Que solo son divisibles entre ellos mismos y el uno",
+        "Que comience la aventura"
+    ]
+
+    @State private var isDialogComplete = false
+    @State private var isGameStarted = false
+    @State private var pulsate = false // Variable para la animación del planeta
+
+    var body: some View {
+        ZStack {
+            BackgroundView()
+                .zIndex(0)
+
+            // El planeta se coloca aquí y se anima para pulsar
+            Image("arim_planeta")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 900, height: 900)
+                .scaleEffect(pulsate ? 1.05 : 1.0)
+                .onAppear {
+                    withAnimation(Animation.easeInOut(duration: 1).repeatForever(autoreverses: true)) {
+                        pulsate.toggle()
+                    }
+                }
+                .zIndex(1) // El planeta está detrás de los botones
+                .offset(y: 400) // Posición actual del planeta
+            ZStack {
+                          
+                          Image(systemName: "star.fill")
+                              .resizable()
+                              .foregroundColor(.yellow)
+                              .frame(width: 60, height: 60)
+                              .offset(x: -300, y: -200)
+                
+                    Image(systemName: "star.fill")
+                    .resizable()
+                    .foregroundColor(.yellow)
+                    .frame(width: 60, height: 60)
+                    .offset(x: -30, y: -450)
+                
+                Image(systemName: "star.fill")
+                    .resizable()
+                    .foregroundColor(.yellow)
+                    .frame(width: 60, height: 60)
+                    .offset(x: 80, y: -300)
+                         
+                          Image(systemName: "star.fill")
+                              .resizable()
+                              .foregroundColor(.yellow)
+                              .frame(width: 60, height: 60)
+                              .offset(x: -180, y: -350)
+                          
+                          Image(systemName: "star.fill")
+                              .resizable()
+                              .foregroundColor(.yellow)
+                              .frame(width: 60, height: 60)
+                              .offset(x: -50, y: -180)
+                          
+                          Image(systemName: "star.fill")
+                              .resizable()
+                              .foregroundColor(.yellow)
+                              .frame(width: 60, height: 60)
+                              .offset(x: 320, y: -290)
+                
+                Image(systemName: "star.fill")
+                    .resizable()
+                    .foregroundColor(.yellow)
+                    .frame(width: 60, height: 60)
+                    .offset(x: 160, y: -140)
+                          
+                          Image(systemName: "star.fill")
+                              .resizable()
+                              .foregroundColor(.yellow)
+                              .frame(width: 60, height: 60)
+                              .offset(x: 190, y: -390)
+                      }
+                      .zIndex(2) // Asegúrate de que las estrellas estén por encima del planeta
+
+
+            VStack(spacing: 0) {
+                Spacer()
+
+                // Contenido de los diálogos o de la pantalla de juego
+                if isDialogComplete {
+                    Image("astronaut")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 450, height: 450)
+                        .padding(.bottom, 20) // Agrega espacio debajo del mono
+
+                    // Botón Jugar
+                    Button("Jugar") {
+                        isGameStarted = true
+                    }
+                    .font(.system(size: 60))
+                    .padding()
+                    .background(Color.secondary)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+                    .zIndex(3) // Asegúrate de que el botón esté encima del planeta
+                    .padding(.top, 20) // Agrega espacio en la parte superior para bajar los botones
+                } else {
+                    ComicChatBubble(text: dialogTexts[dialogIndex])
+                        .padding(.horizontal)
+                    
+                    // Botón Siguiente
+                    Button("Siguiente") {
+                        if dialogIndex < dialogTexts.count - 1 {
+                            dialogIndex += 1
+                        } else {
+                            isDialogComplete = true
+                        }
+                    }
+                    .font(.system(size: 40))
+                    .foregroundColor(Color.white)
+                    .background(Color.secondary)
+                    .cornerRadius(10)
+                    .padding()
+                    .zIndex(3) // Asegúrate de que el botón esté encima del planeta
+                }
+
+                Spacer()
+            }
+            .zIndex(2) // Asegúrate de que el VStack esté encima del planeta
+            .offset(y: 260) // Ajusta la posición vertical del VStack para que los botones estén más abajo
+
+            .fullScreenCover(isPresented: $isGameStarted, content: {
+                ArithmeticEx1View()
+            })
+        }
+    }
+}
+
+struct StarEx1View_Previews: PreviewProvider {
+    static var previews: some View {
+        StarEx1View()
+    }
+}
+
+struct ComicChatBubble: View {
+    let text: String
+
+    var body: some View {
+        VStack {
+            HStack {
+                Spacer()
+                Text(text)
+                    .foregroundColor(.red)
+                    .font(.system(size: 35))
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .background(RoundedRectangle(cornerRadius: 70).fill(Color.white))
+                Spacer()
+            }
+            // No necesitas verificar contra dialogTexts.last aquí ya que no hay referencia al arreglo
+            Image("astronaut")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 400, height: 400)
+        }
+        .frame(maxWidth: .infinity, alignment: .center)
+        .padding()
+    }
+}
